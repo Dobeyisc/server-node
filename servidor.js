452085f9471197edb2efaa1ce7b9a24810cb67aa
Dobeyisc <109563592+Dobeyisc@users.noreply.github.com>
2023-09-19 14:56:11 -0500

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -1,27 +1,39 @@
 const http = require('http');
-const chalk = require('chalk');
+const nodemon = require('nodemon');
 
-// Constantes para el host y el puerto
-const host = '127.0.0.1';
-const puerto = 8000; // Puedes cambiarlo a 8080 si lo prefieres
+// Arreglo de tareas con objetos
+const tareas = [
+  { id: 1, descripcion: 'Sacar al perro', completado: true },
+  { id: 2, descripcion: 'lavar ropa, hacer aseo, y comida', completado: true },
+  { id: 3, descripcion: 'Escribir código', completado: false },
+];
 
-// Crear el servidor HTTP
-const servidor = http.createServer((solicitud, respuesta) => {
-  // Configurar encabezado de respuesta
-  respuesta.setHeader('Content-Type', 'application/json');
-
-  // Objeto JSON de respuesta
-  const respuestaJSON = {
-    nombre: 'Esteban',
-    mensaje: 'Hola desde mi servidor HTTP en Node.js',
-  };
-
-  // Enviar la respuesta JSON
-  respuesta.end(JSON.stringify(respuestaJSON));
+const servidor = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/tareas') {
+    // Configura el encabezado de respuesta para JSON
+    res.setHeader('Content-Type', 'application/json');
+    
+    // Envía el arreglo de tareas como JSON
+    res.end(JSON.stringify(tareas));
+  } else {
+    // Maneja rutas no válidas
+    res.statusCode = 404;
+    res.end('Ruta no encontrada');
+  }
 });
 
-// Escuchar en el puerto y el host especificados
-servidor.listen(puerto, host, () => {
-  const mensaje = `El servidor está escuchando en ${chalk.green(`http://${host}:${puerto}/`)}`;
-  console.log(mensaje);
+// Inicia el servidor en el puerto 3000
+servidor.listen(3000, () => {
+  console.log('El servidor está escuchando en http://localhost:3000/');
+});
+
+// Configura nodemon para reiniciar automáticamente el servidor al hacer cambios en el código
+nodemon({
+  script: 'servidor.js',
+  ext: 'js',
+  watch: '.'
+});
+
+nodemon.on('restart', () => {
+  console.log('El servidor se ha reiniciado debido a cambios en el código.');
 });
